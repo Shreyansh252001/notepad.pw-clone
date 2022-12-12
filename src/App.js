42b04a86b3fe3cfbc4753b6fd01ebb62e6aa935e
Shreyansh Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-12-12 12:06:22 +0530

    navbar started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, components } from 'react';
 
-function App() {
+
+// import { ArrowRight } from 'react-bootstrap-icons';
+// import * as icons from 'react-bootstrap-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+export default function App() {
+  const [showBasic, setShowBasic] = useState(false);
   return (
     <div className="App">
+
+      <head>
+        <link href="http://man.hubwiz.com/docset/IonIcons.docset/Contents/Resources/Documents/" />
+      </head>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <body>
+
+      <nav class="navbar navbar-expand-lg bg-light navbar-dark">
+        
+      <div class="container">
+      
+        {/* <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" aria-label="Toggle navigation"
+        aria-controls="navbarSupportedContent" aria-expanded="false" data-mdb-target="#navbarSupportedContent" >
+            <i class="fas fa-bars"></i>
+        </button> */}
+
+     
+        <div class="flex-row" id="navbarSupportedContent">
+           
+
+            <div class="notepad_logo d-flex flex-row fl-left me-1" title="Click here for Google Search">
+                <img class="main-logo" src="images/logo-dark.png" alt="logo" height="100%" width="90px"/>
+            </div>
+
+            
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 fl-right">
+
+
+
+            {/* <ul class="navbar-nav d-flex flex-row me-1"> */}
+
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link mx-1" href="#">
+                    
+                    <iconify-icon icon="ion:lock-closed"></iconify-icon>
+                   
+                    </a>
+                </li>
+
+                <li class="nav-item me-3 me-lg-0">
+                      <a class="nav-link mx-1 text-white" href="#">
+
+                      <i class="fab fa-imdb fa-5x">
+
+                      </i>
+
+                      </a>
+                </li>
+
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link mx-1" href="#">
+                      <i class="fas fa-image">
+
+                      </i>
+                    </a>
+                </li>
+
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link mx-1" href="#">
+                      <i class="fab fa-google-plus-g">
+
+                      </i>
+                    </a>
+                </li>
+
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link mx-1" href="#">
+                      <i class="fas fa-image">
+
+                      </i>
+                    </a>
+                </li>
+
+                 <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link mx-1" href="#">
+                      <i class="fas fa-image">
+
+                      </i>
+                    </a>
+                </li>
+
+               <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link mx-1" href="#">
+                      <i class="fas fa-image">
+
+                      </i>
+                    </a>
+                </li>
+
+
+                 <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link mx-1" href="#">
+                      <i class="fas fa-image">
+
+                      </i>
+                    </a>
+                </li>
+
+
+            </ul>
+        </div>
     </div>
+   
+</nav>
+
+
+      </body>
+
+    </div>
+
   );
 }
 
-export default App;
+/*
+export default App;*/
---
+++ src/components/Navbar.js
@@ -0,0 +1,4 @@
+// import {React} from 'react';
+// import App from '../App';
+// import 'src\App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
